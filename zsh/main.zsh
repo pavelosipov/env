0021ad842d77f9cc28ad60c54f4b0ab2d7dc80ed
Pavel Osipov <pavel.osipov@outlook.com>
2013-07-02 23:33:31 +0400

    Add prompts.zsh with orange prompt setup.

--- zsh/main.zsh
+++ zsh/main.zsh
@@ -10,10 +10,12 @@ ZSH_ENV_PATH=~/env/zsh
 ZSH_CONFIG_PATH=$ZSH_ENV_PATH/config
 ZSH_DIRCOLORS_PATH=$ZSH_ENV_PATH/dircolors/dircolors.256dark
 
+# Sourcing configs
 source $ZSH_CONFIG_PATH/aliases.zsh
 source $ZSH_CONFIG_PATH/options.zsh
 source $ZSH_CONFIG_PATH/bindings.zsh
 source $ZSH_CONFIG_PATH/completions.zsh
+source $ZSH_CONFIG_PATH/prompts.zsh
 
 source $ZSH_ENV_PATH/syntax-highlighting/zsh-syntax-highlighting.zsh
 ZSH_HIGHLIGHT_STYLES[path]='fg=blue'
