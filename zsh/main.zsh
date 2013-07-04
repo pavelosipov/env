80477893d645ae7436cb2681a9d8afce5f9f207d
Pavel Osipov <p.osipov@corp.mail.ru>
2013-07-04 11:03:48 +0400

    Add history variables

--- zsh/main.zsh
+++ zsh/main.zsh
@@ -3,12 +3,16 @@
 # Copyright (c) Pavel Osipov <posipov84@gmail.com> 2013.
 # All rights reserved.
 
-ZSH_DEFAULT_USER="furj"
-
 # Paths
 ZSH_ENV_PATH=~/env/zsh
 ZSH_CONFIG_PATH=$ZSH_ENV_PATH/config
 ZSH_DIRCOLORS_PATH=$ZSH_ENV_PATH/dircolors/dircolors.256dark
+
+# History
+HISTFILE=~/.zsh_history
+HISTSIZE=2000
+SAVEHIST=2000
+DIRSTACKSIZE=20
 
 # Sourcing configs
 source $ZSH_CONFIG_PATH/aliases.zsh
